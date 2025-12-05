5279741dfa52d1a6f8169a708c29f20ae3a12f33
shalini <s.rathore43@gmail.com>
2025-12-05 19:16:55 +0530

    changes in movie_lisst.html and views of revision app  ... pagination added

--- myproject/revision/views.py
+++ myproject/revision/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render,get_object_or_404
-
 from .models import Movie
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
 def movie_list(request):
-    movies = Movie.objects.all()
+    object_list  = Movie.objects.all()
+    
+    paginator = Paginator(object_list,3) #3 movies per page
+    page = request.GET.get('page')
+    try:
+        movies =paginator.page(page)
+    except PageNotAnInteger: #if page is not a integer -> show first page
+        movies = paginator.page(1)
+    except EmptyPage: #if page is out of range -> show lastpage
+        movies = paginator.page(paginator.num_pages)
+        
     context ={'movies':movies}
     return render(request,"revision/movie_list.html",context)
+
+
+
+
 def movie_details_view(request,pk):
     movie = get_object_or_404(Movie,pk=pk)
     context = {'movie':movie}
